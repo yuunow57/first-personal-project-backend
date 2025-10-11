333461240c4d3e7b914d0900883e37b6f1edc4e4
yuunow57 <yharche40@gmail.com>
2025-10-11 21:41:25 +0900

    Day3.1 트랜잭션 적용

--- controllers/tradeController.js
+++ controllers/tradeController.js
@@ -1,51 +1,63 @@
 import asyncHandler from "express-async-handler";
 import Trade from "../models/Trade.js";
 import User from "../models/User.js"; // 거래의 주인을 확인하기 위함
+import sequelize from "../config/database.js";
 
 // ✅ 거래 생성 (Create)
 export const createTrade = asyncHandler(async (req, res) => {
     const { userId, type, coinName, quantity, price, } = req.body;
     
-    const user = await User.findByPk(userId); 
-    if (!user) // userId가 없을시 에러처리
-        return res.status(404).json({ message: "❌ 해당 사용자를 찾을 수 없습니다." });
-
-    // 유효성 검사 부분
-    if (!type || !coinName || !quantity || !price) // 필수 입력값 누락 확인
-        return res.status(400).json({ message: "❌ 필수 입력값이 누락되었습니다." });
-
-    if (quantity <= 0 || price <= 0) // 수량과 가격 음수체크
-        return res.status(400).json({ message: "❌ 수량과 가격은 0보다 커야 합니다." });
+    const transaction = await sequelize.transaction(); // 트랜잭션 생성
+    try {
+        const user = await User.findByPk(userId, { transaction }); // 트랜잭션 처리
+        if (!user) // userId가 없을시 에러처리
+            return res.status(404).json({ message: "❌ 해당 사용자를 찾을 수 없습니다." });
     
-    if (!["buy", "sell"].includes(type)) // 거래 유형 검사 ( .includes(type) : type이 해당 배열 안에 포함되어 있는지를 true/false로 반환 )
-        return res.status(400).json({ message: "❌ 잘못된 거래 유형입니다." });
+        // 유효성 검사 부분
+        if (!type || !coinName || !quantity || !price) // 필수 입력값 누락 확인
+            return res.status(400).json({ message: "❌ 필수 입력값이 누락되었습니다." });
+    
+        if (quantity <= 0 || price <= 0) // 수량과 가격 음수체크
+            return res.status(400).json({ message: "❌ 수량과 가격은 0보다 커야 합니다." });
+        
+        if (!["buy", "sell"].includes(type)) // 거래 유형 검사 ( .includes(type) : type이 해당 배열 안에 포함되어 있는지를 true/false로 반환 )
+            return res.status(400).json({ message: "❌ 잘못된 거래 유형입니다." });
+    
+        const totalAmount = quantity * price;
+    
+        // 잔액 증감 처리 부분
+        if (type === "buy") {
+            if (user.balance < totalAmount)
+                return res.status(400).json({ message: "❌ 잔액이 부족합니다." });
+            user.balance = Number(user.balance) - totalAmount;
+        } else if (type === "sell") {
+            user.balance = Number(user.balance) + totalAmount; // decimal 타입은 sequelize에서 문자열로 반환되기 때문에 Number로 감싸줘야 함
+        }
 
-    const totalAmount = quantity * price;
+        // if (type === "sell" && coinName === "BTC")  // 트랜잭션 테스트 코드
+        //     throw new Error("테스트용 강제 오류 발생");
 
-    // 잔액 증감 처리 부분
-    if (type === "buy") {
-        if (user.balance < totalAmount)
-            return res.status(400).json({ message: "❌ 잔액이 부족합니다." });
-        user.balance = Number(user.balance) - totalAmount;
-    } else if (type === "sell") {
-        user.balance = Number(user.balance) + totalAmount;
+        await user.save({ transaction }); // 증감 처리 저장, 트랜잭션 처리
+        
+        const newTrade = await Trade.create({
+            userId,
+            type,
+            coinName,
+            quantity,
+            price,
+            totalAmount,
+        }, { transaction }); // 트랜잭션 처리
+        
+        await transaction.commit(); // 성공 시 반영
+
+        res.status(201).json({
+            message: "✅ 거래 등록 완료",
+            trade: newTrade,
+        });
+    } catch (error) {
+        await transaction.rollback(); // 실패 시 되돌림
+        res.status(500).json({ message: " ❌ 거래 중 오류 발생", error: error.message });
     }
-
-    await user.save(); // 증감 처리 저장
-    
-    const newTrade = await Trade.create({
-        userId,
-        type,
-        coinName,
-        quantity,
-        price,
-        totalAmount,
-    });
-
-    res.status(201).json({
-        message: "✅ 거래 등록 완료",
-        trade: newTrade,
-    });
 });
 
 // ✅ 모든 거래 내역 조회 (Read All)
