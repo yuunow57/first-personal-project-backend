9e210bcea2ca792bbffbe161b97815d99d10a846
yuunow57 <yharche40@gmail.com>
2025-10-24 14:32:53 +0900

    Day13: 배포 전 마지막 점검

--- config/database.js
+++ config/database.js
@@ -5,7 +5,15 @@ dotenv.config();
 /* 해당 함수는 프로세스에서 한 번만 호출하면 .env 값들이 전역 객체인 process.env에 담기기 떄문에 
 다른 파일에서 dotenv를 참조하지않고 곧바로 process.env로 .env에 접근 할 수 있음 */
 
-const sequelize = new Sequelize(
+const sequelize = process.env.DATABASE_URL
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: "mysql",
+        dialectOptions: {
+            ssl: { rejectUnauthorized: false },
+        },
+        logging: false,
+    })
+    : new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
