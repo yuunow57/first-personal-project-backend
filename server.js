8196f8ce5701a313789728638bb689f7c96307a1
yuunow57 <yharche40@gmail.com>
2025-10-24 15:24:45 +0900

    Day13: 배포 전 마지막 점검

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",  //프론트 주소 (Vite 기본 포트)
+    origin: [
+        "http://localhost:5173",  // 로컬 프론트 주소 (Vite 기본 포트)
+        "https://first-personal-project-frontend.vercel.app",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
